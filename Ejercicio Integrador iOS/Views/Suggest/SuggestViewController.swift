a30a7bd595e18af678c41056f202c18c94f5958a
Francisco <francisco.nasich@mercadolibre.com>
2022-06-06 18:01:41 -0300

    Added some Views groups

---
+++ Ejercicio Integrador iOS/Views/Suggest/SuggestViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SuggestViewController.swift
+//  Ejercicio Integrador iOS
+//
+//  Created by Francisco Tomas Nasich on 06/06/2022.
+//
+
+import UIKit
+
+class SuggestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
