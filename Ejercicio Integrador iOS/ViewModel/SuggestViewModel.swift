f8ca0e407102b030a289f8b2e47214854e38ebed
Francisco Nasich <francisco.nasich@mercadolibre.com>
2022-06-08 10:39:03 -0300

    Suggest view (#6)
    * Added the title of SuggestView  conection
    
    * Added SuggestViewController

---
+++ Ejercicio Integrador iOS/ViewModel/SuggestViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SuggestViewModel.swift
+//  Ejercicio Integrador iOS
+//
+//  Created by Francisco Tomas Nasich on 07/06/2022.
+//
+
+import Foundation
+
+class SuggestViewModel{
+    
+}
--- Ejercicio Integrador iOS/Views/Suggest/SuggestViewController.swift
+++ Ejercicio Integrador iOS/Views/Suggest/SuggestViewController.swift
@@ -9,13 +9,34 @@ import UIKit
 
 class SuggestViewController: UIViewController {
     
+    @IBOutlet weak var suggestTitle: UILabel!
     @IBOutlet weak var participantsNumber: UILabel!
     @IBOutlet weak var price: UILabel!
     
+    var viewModel: SuggestViewModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.viewModel = SuggestViewModel(service: SuggestService())
     }
+    
     @IBAction func tryAnotherButton(_ sender: Any) {
     }
     
+    //MARK: Price
+    func getPrice(price: Double) -> String {
+        switch price {
+        case 0:
+            return "Free"
+        case 0.1...0.3:
+            return "Low"
+        case 0.3...0.6:
+            return "Medium"
+        case 0.7...0.9 :
+            return "High"
+        default:
+            return "Free"
+        }
+    }
+
 }
