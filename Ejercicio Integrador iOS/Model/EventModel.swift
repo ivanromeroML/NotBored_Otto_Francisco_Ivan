dcea36ec163e1a698d950cbba3d58fdd8fa96210
Oscar Alejandro Rafael Cabanillas <oscar.cabanillas@mercadolibre.com>
2022-06-08 10:08:49 -0300

    rebase

---
+++ Ejercicio Integrador iOS/Model/EventModel.swift
@@ -0,0 +1,21 @@
+//
+//  EventModel.swift
+//  Ejercicio Integrador iOS
+//
+//  Created by Oscar Alejandro Rafael Cabanillas on 07/06/2022.
+//
+
+import Foundation
+
+// MARK: EventM struct
+struct EventM: Decodable {
+    
+    // MARK: - Properties
+    let activity: String
+    let type: String
+    let participants: Int
+    let price: Int
+    let link: String
+    let key: String
+    let accessibility: Float
+}
